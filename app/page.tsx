d90fa8bbb2b6d786df684a49094d0d4d6b458124
JoshJHull <64508673+JoshJHull@users.noreply.github.com>
2025-09-23 13:01:10 +0100

    Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import * as motion from "motion/react-client"
+import * as motion from "motion/react-client";
 import RaceImage from "../public/home/race.jpg";
 import styles from "./home.module.css";
 import Link from "next/link";
@@ -7,18 +7,35 @@ import Link from "next/link";
 export default function Home() {
     return (
         <>
-            <div className={"flex w-full justify-center text-white bg-gradient-to-t from-[#171738] to-black"}>
+            <div
+                className={
+                    "flex w-full justify-center text-white bg-gradient-to-t from-[#171738] to-black"
+                }
+            >
                 <div className={"flex-col max-w-[800] min-w-[800] pt-[100]"}>
                     <Link href={"/race"}>
-                        <motion.div className={styles.border} whileHover={{ scale: 1.03 }}>
+                        <motion.div
+                            className={styles.border}
+                            whileHover={{ scale: 1.03 }}
+                        >
                             <div className={"flex bg-[#171738]"}>
                                 <div className={"w-1/2"}>
-                                    <Image src={RaceImage} alt={"Picture of ships racing"}></Image>
+                                    <Image
+                                        src={RaceImage}
+                                        alt={"Picture of ships racing"}
+                                    ></Image>
                                 </div>
-                                <div className={"flex-col space-y-4 w-1/2 pl-4 p-2"}>
-                                    <p className={styles.underline}>Quantum Race</p>
+                                <div
+                                    className={
+                                        "flex-col space-y-4 w-1/2 pl-4 p-2"
+                                    }
+                                >
+                                    <p className={styles.underline}>
+                                        Quantum Race
+                                    </p>
                                     <p>
-                                        Pit 2 ships against each-other in a head to head quantum travel race.
+                                        Pit 2 ships against each-other in a head
+                                        to head quantum travel race.
                                     </p>
                                 </div>
                             </div>
@@ -27,6 +44,5 @@ export default function Home() {
                 </div>
             </div>
         </>
-      )
+    );
 }
-
